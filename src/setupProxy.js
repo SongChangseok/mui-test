ff7873826c5d03e47a0414f64e659cc0dd02847b
cssong <a1373052011@gmail.com>
2022-05-26 17:55:23 +0900

    setupProxy 설정 변경

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,13 +2,12 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/dstk",
-    createProxyMiddleware({
+    createProxyMiddleware("/dstk", {
       target: process.env.REACT_APP_API_HOST,
       changeOrigin: true,
-      // pathRewrite: {
-      //   "/dstk": "",
-      // },
+      pathRewrite: {
+        "/dstk": "",
+      },
     })
   );
 };
